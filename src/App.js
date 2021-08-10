f93417351bd28448304344a10aeea950f607d072
Sanu Shekhar Azad <sanuk1294@gmail.com>
2021-08-10 09:00:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component.jsx'
 
 function App() {
   return (
     <div>
-       <Homepage />
+      <switch>
+       <Route exact path='/' component={Homepage} />
+       <Route path='/Shop' component={ShopPage} />
+      </switch>
     </div>
   );
 }
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,12 +2,45 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.styles.scss';
+import './homepage.styles.scss'
 
-const Homepage = () => (
-    <div className='homepage'>
-    <Directory />
+const HomePage = () => (
+    <div className = 'homepage'>
+        {/* <div className = 'directory-menu'>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>HATS</h1>
+                    <span className = 'subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>JACKETS</h1>
+                    <span className = 'subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>SNEAKERS</h1>
+                    <span className = 'subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>WOMENS</h1>
+                    <span className = 'subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+            <div className = 'menu-item'>
+                <div className = 'content'>
+                    <h1 className = 'title'>MENS</h1>
+                    <span className = 'subtitle'>SHOP NOW</span>
+                </div>
+            </div>
+        </div> */}
+
+        <Directory />
     </div>
 );
 
-export default Homepage;
+export default HomePage;
